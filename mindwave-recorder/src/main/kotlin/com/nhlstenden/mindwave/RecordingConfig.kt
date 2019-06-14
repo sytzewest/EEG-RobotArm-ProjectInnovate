c05fe4f8a4e3f4570511f6d30fd253d61003a3c4
Daniil Sapozhnikov <daniil.sapozhnikov@student.nhlstenden.com>
2019-06-14 10:46:59 +0200

    Remove recording rate, just record all data in the interval

--- mindwave-recorder/src/main/kotlin/com/nhlstenden/mindwave/RecordingConfig.kt
+++ mindwave-recorder/src/main/kotlin/com/nhlstenden/mindwave/RecordingConfig.kt
@@ -3,11 +3,10 @@ package com.nhlstenden.mindwave
 //configuration for the recorder app
 //recording rate is rate at which data wil
 data class RecordingConfig(
-    val recordingRate: Double,
     val modeDuration: Double,
     val modes: List<String>
 ) {
     companion object {
-        val DEFAULT = RecordingConfig(100.0, 5.0, listOf("left", "none", "right"))
+        val DEFAULT = RecordingConfig(5.0, listOf("left", "none", "right"))
     }
 }
